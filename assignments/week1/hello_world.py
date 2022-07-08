a96ff4714971b0957e2c8c9a760cddaa13954953
alora1976 <39773598+alora1976@users.noreply.github.com>
2022-07-08 11:57:46 -0700

    initial commit

--- assignments/week1/hello_world.py
+++ assignments/week1/hello_world.py
@@ -1,1 +1,15 @@
 print("hello world")
+your_name="Lori"
+print("hello", your_name)
+print("hello " + your_name)
+fave_number=7
+print("hello",fave_number)
+print("hello "+ str(fave_number))
+fave_food1="pizza"
+fave_food2="ice cream"
+print("I love", fave_food1, "and", fave_food2)
+fave_food1="pizza"
+fave_food2="ice cream"
+print(f"I love", fave_food1, "and", fave_food2)
+
+
