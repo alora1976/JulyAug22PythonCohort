53458c9bebd25a4a4512236f96b38e4915261835
Lori Trindade <39773598+alora1976@users.noreply.github.com>
2022-07-31 10:45:47 -0700

    changes

--- assignments/week4/checkerboard/server.py
+++ assignments/week4/checkerboard/server.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html",row=8,col=8,color_one='red',color_two='black')
+    return render_template("index.html",row=8,col=8,color_one='pink',color_two='purple')
 
 
 @app.route('/<int:x>')
 def row(x):
-    return render_template("index.html",row=x,col=8,color_one='red',color_two='black')
+    return render_template("index.html",row=x,col=8,color_one='pink',color_two='purple')
 
 @app.route('/<int:x>/<int:y>')
 def row_col(x,y):
-    return render_template("index.html",row=x,col=y,color_one='red',color_two='black')
+    return render_template("index.html",row=x,col=y,color_one='pink',color_two='purple')
 
 @app.route('/<int:x>/<int:y>/<string:one>')
 def row_col_one(x,y,one):
-    return render_template("index.html",row=x,col=y,color_one=one,color_two='black')
+    return render_template("index.html",row=x,col=y,color_one=one,color_two='purple')
 
 @app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
 def row_col_two(x,y,one,two):
